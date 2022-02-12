27674669feac384b497d5af882ed40cc800e691d
EC2 Default User <ec2-user@ip-172-31-17-177.ec2.internal>
2022-02-12 07:24:36 +0000

    4-課題48

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,18 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new
-    @books= Book.all
+    @books = Book.all
     @user = current_user
   end
   
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+    redirect_to book_path(@book.id), notice: "You have created book successfully."
+    else
+    redirect_to books_path, flash: {error: @book.errors.full_messages}
+    end
   end
   
   def show
@@ -23,8 +26,11 @@ class BooksController < ApplicationController
   
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: "You have updated book successfully."
+    else
+    render :edit
+    end
   end
   
   
