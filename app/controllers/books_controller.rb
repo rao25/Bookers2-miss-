7791a2bc21c3d0d48d44d7b235197e372260c3c1
EC2 Default User <ec2-user@ip-172-31-17-177.ec2.internal>
2022-02-14 07:01:46 +0000

    4-課題:不要のアクション削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -51,10 +51,4 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body, :image, :caption)
   end
-  
-  def correct_user
-    @book = Book.find(params[:id])
-    @user = @book.user
-    redirect_to(books_path) unless @user == current_user
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,9 +35,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(user_path(current_user.id)) unless @user == current_user
-  end
 end
